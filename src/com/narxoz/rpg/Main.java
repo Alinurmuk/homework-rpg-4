fb4a4ec45405f186ecb1b4f968f289da36617d90
Alinurmuk <alimur.mukazhan@narxoz.kz>
2026-03-16 00:31:57 +0500

    main

--- src/com/narxoz/rpg/Main.java
+++ src/com/narxoz/rpg/Main.java
@@ -1,65 +1,35 @@
 package com.narxoz.rpg;
-
-import com.narxoz.rpg.battle.RaidEngine;
-import com.narxoz.rpg.battle.RaidResult;
-import com.narxoz.rpg.bridge.AreaSkill;
-import com.narxoz.rpg.bridge.FireEffect;
-import com.narxoz.rpg.bridge.IceEffect;
-import com.narxoz.rpg.bridge.SingleTargetSkill;
-import com.narxoz.rpg.bridge.Skill;
-import com.narxoz.rpg.composite.CombatNode;
-import com.narxoz.rpg.composite.EnemyUnit;
-import com.narxoz.rpg.composite.HeroUnit;
-import com.narxoz.rpg.composite.PartyComposite;
-import com.narxoz.rpg.composite.RaidGroup;
+import com.narxoz.rpg.battle.*;
+import com.narxoz.rpg.bridge.*;
+import com.narxoz.rpg.composite.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("=== Homework 4 Demo: Bridge + Composite ===\n");
+        PartyComposite squad = new PartyComposite("Frontline");
+        squad.add(new HeroUnit("Knight", 100, 20));
+        squad.add(new HeroUnit("Archer", 80, 15));
 
-        // TODO: build leaves
-        HeroUnit warrior = new HeroUnit("Arthas", 140, 30);
-        HeroUnit mage = new HeroUnit("Jaina", 90, 40);
-        EnemyUnit goblin = new EnemyUnit("Goblin", 70, 20);
-        EnemyUnit orc = new EnemyUnit("Orc", 120, 25);
+        RaidGroup army = new RaidGroup("Crusaders");
+        army.add(squad);
+        army.add(new HeroUnit("Wizard", 60, 40));
 
-        // TODO: build composite hierarchy (nested)
-        PartyComposite heroes = new PartyComposite("Heroes");
-        heroes.add(warrior);
-        heroes.add(mage);
+        EnemyUnit boss = new EnemyUnit("Dragon", 800, 50);
 
-        PartyComposite frontline = new PartyComposite("Frontline");
-        frontline.add(goblin);
-        frontline.add(orc);
+        Skill meteor = new AreaSkill("Meteor", 40, new FireEffect());
+        Skill claw = new SingleTargetSkill("Claw", 35, new ShadowEffect());
 
-        RaidGroup enemies = new RaidGroup("Enemy Raid");
-        enemies.add(frontline);
+        System.out.println("--- INITIAL ---");
+        army.printTree("");
+        boss.printTree("");
 
-        System.out.println("--- Team Structures ---");
-        heroes.printTree("");
-        enemies.printTree("");
-
-        // TODO: Bridge combinations
-        Skill slashFire = new SingleTargetSkill("Slash", 20, new FireEffect());
-        Skill slashIce = new SingleTargetSkill("Slash", 20, new IceEffect());
-        Skill stormFire = new AreaSkill("Storm", 15, new FireEffect());
-
-        System.out.println("\n--- Bridge Preview ---");
-        System.out.println(slashFire.getSkillName() + " using " + slashFire.getEffectName());
-        System.out.println(slashIce.getSkillName() + " using " + slashIce.getEffectName());
-        System.out.println(stormFire.getSkillName() + " using " + stormFire.getEffectName());
-
-        // TODO: run raid
         RaidEngine engine = new RaidEngine().setRandomSeed(42L);
-        RaidResult result = engine.runRaid(heroes, enemies, slashFire, stormFire);
+        RaidResult result = engine.runRaid(army, boss, meteor, claw);
 
-        System.out.println("\n--- Raid Result ---");
-        System.out.println("Winner: " + result.getWinner());
-        System.out.println("Rounds: " + result.getRounds());
-        for (String line : result.getLog()) {
-            System.out.println(line);
-        }
+        System.out.println("\n--- LOG ---");
+        result.getLog().forEach(System.out::println);
 
-        System.out.println("\n=== Demo Complete ===");
+        System.out.println("\n--- FINAL ---");
+        army.printTree("");
+        boss.printTree("");
     }
 }
